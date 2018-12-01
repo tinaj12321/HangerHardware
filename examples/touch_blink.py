f368bde8008f0bfc049616cf35904f3b6c7cd3b8
Daniel Pinto <dpinto@stevens.edu>
2018-12-01 20:46:56 +0000

    hardware finally done todo twilio

--- examples/touch_blink.py
+++ examples/touch_blink.py
@@ -10,7 +10,7 @@ yellow_led = mraa.Gpio(33)
 yellow_led.dir(mraa.DIR_OUT)
 yellow_led.write(0)
 
-led3 = mraa.Gpio(34)
+led3 = mraa.Gpio(27)
 led3.dir(mraa.DIR_OUT)
 led3.write(0)
 
@@ -20,25 +20,56 @@ touch.dir(mraa.DIR_IN)
 toggle = False
 
 while True:
-    touchButton = int(touch.read())
-    if(touchButton == 1):
+    if(int(touch.read()) == 1):
         print("touch!")
         toggle = not toggle
+        was_touched = False
+        timeout = time.time() + time_delay
+        time.sleep(1)
         if toggle:
-            time.sleep(time_delay)
-            green_led.write(1)
-            print("green")
-            time.sleep(time_delay)
-            green_led.write(0)
-            yellow_led.write(1)
-            print("yellow")
-            time.sleep(time_delay)
-            yellow_led.write(0)
-            led3.write(1)
-            print("last")
-            #TODO send signal to REST endpoint
+            while timeout > time.time():
+                if int(touch.read()) == 1:
+                    was_touched = True
+                    break
+            if was_touched:
+                toggle = not toggle
+                print("reset: clothing used") 
+                time.sleep(1)
+            elif toggle:
+                green_led.write(1)
+                print("green")
+                timeout = time.time() + time_delay
+                time.sleep(1)
+                if toggle:
+                    while timeout > time.time():
+                        if int(touch.read()) == 1:
+                            was_touched = True
+                            break
+                    green_led.write(0)
+                    if was_touched:
+                        toggle = not toggle
+                        print("reset: clothing used") 
+                        time.sleep(1)
+                    elif toggle: 
+                        yellow_led.write(1)
+                        print("yellow")
+                        timeout = time.time() + time_delay
+                        time.sleep(1)
+                        if toggle:
+                            while timeout > time.time():
+                                if int(touch.read()) == 1:
+                                    was_touched = True
+                                    break
+                            yellow_led.write(0)
+                            if was_touched:
+                                toggle = not toggle
+                                print("reset: clothing used")
+                                time.sleep(1)
+                            elif toggle: 
+                                led3.write(1)
+                                print("last")
+                                #TODO send signal to REST endpoint
         else:
-            time.sleep(time_delay)
             green_led.write(0)
             yellow_led.write(0)
             led3.write(0)
