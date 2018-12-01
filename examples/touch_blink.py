e572472716fe6bd9c5470f898ce9a1338f8878bc
Daniel Pinto <dpinto@stevens.edu>
2018-12-01 07:25:11 +0000

    added time delay

--- examples/touch_blink.py
+++ examples/touch_blink.py
@@ -1,17 +1,24 @@
 import mraa
+import time
 print(mraa.getVersion())
 
 led = mraa.Gpio(31)
 led.dir(mraa.DIR_OUT)
 led.write(0)
 
+time_delay = 3
 touch = mraa.Gpio(29)
 touch.dir(mraa.DIR_IN)
+toggle = False
 
 while True:
     touchButton = int(touch.read())
     if(touchButton == 1):
-        led.write(1)
-    else:
-        led.write(0)
-    print("touch   " + str(touchButton) + "  led " + str(led.read()))
+        print("touch!")
+        print("touch   " + str(touchButton) + "  led " + str(led.read()))
+        toggle = not toggle
+        time.sleep(time_delay)
+        if toggle:
+            led.write(1)
+        else:
+            led.write(0)
