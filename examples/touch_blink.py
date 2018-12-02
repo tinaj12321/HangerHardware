9baec610f22d8e683c7bbb600ef70695d6186a37
Tina Janulis <tina.janulis@rutgers.edu>
2018-12-02 03:59:09 +0000

    twilio added. minor bug at end. google maps perhaps?

--- examples/touch_blink.py
+++ examples/touch_blink.py
@@ -1,5 +1,13 @@
 import mraa
 import time
+from twilio.rest import Client
+
+account_sid = 'AC52b6653b756bcb8853e3a42773bdf15c'
+auth_token = 'b6631ae520bf37f5c02de8c00bf3d8ed'
+client = Client(account_sid,auth_token)
+twilio_num = '+18454456307'
+my_num='+19738309712'
+
 print(mraa.getVersion())
 
 green_led = mraa.Gpio(31)
@@ -22,24 +30,37 @@ toggle = False
 while True:
     if(int(touch.read()) == 1):
         print("touch!")
+        welcomeMessage=client.messages.create(
+            to=my_num,
+            from_=twilio_num,
+            body="Something has been hung up here. We're keeping track of your usage :)"
+        )
         toggle = not toggle
         was_touched = False
         timeout = time.time() + time_delay
-        time.sleep(1)
+        time.sleep(5)
         if toggle:
             while timeout > time.time():
                 if int(touch.read()) == 1:
                     was_touched = True
                     break
             if was_touched:
+                messageReset1 = client.messages.create(
+                    to=my_num,
+                    from_=twilio_num,
+                    body="Clothing taken at " + str(time.ctime(int(time.time())))+". You hung this up recently so you must wear this a lot!")
+                print("taken from hanger")
                 toggle = not toggle
-                print("reset: clothing used") 
-                time.sleep(1)
+                time.sleep(5)
             elif toggle:
+                messageGreen = client.messages.create(
+                    to=my_num,
+                    from_=twilio_num,
+                    body="It's getting into the first week you've left this in the closet... It's currently "+str(time.ctime(int(time.time())))+". You hung this up recently so you must wear this a lot!") 
                 green_led.write(1)
                 print("green")
                 timeout = time.time() + time_delay
-                time.sleep(1)
+                time.sleep(5)
                 if toggle:
                     while timeout > time.time():
                         if int(touch.read()) == 1:
@@ -47,14 +68,20 @@ while True:
                             break
                     green_led.write(0)
                     if was_touched:
+
+                        print("clothing placed on hanger")
                         toggle = not toggle
-                        print("reset: clothing used") 
-                        time.sleep(1)
-                    elif toggle: 
+                        time.sleep(5)
+                    elif toggle:
+                        yellowMessage=client.messages.create(
+                            to=my_num,
+                            from_=twilio_num,
+                            body="It's now the second week this item hasn't been used. The light will be yellow up until 5 weeks. date is: "+str(time.ctime(int(time.time()))))
+
                         yellow_led.write(1)
                         print("yellow")
                         timeout = time.time() + time_delay
-                        time.sleep(1)
+                        time.sleep(5)
                         if toggle:
                             while timeout > time.time():
                                 if int(touch.read()) == 1:
@@ -62,14 +89,26 @@ while True:
                                     break
                             yellow_led.write(0)
                             if was_touched:
+                                messageReset3 = client.messages.create(
+                                    to=my_num,
+                                    from_=twilio_num,
+                                    body="You wore it between two and five weeks! It hasn't been ages but still don't let this get to the back of the closet. This was removed at "+str(time.ctime(int(time.time()))))
+
                                 toggle = not toggle
-                                print("reset: clothing used")
-                                time.sleep(1)
+
+                                time.sleep(5)
                             elif toggle: 
                                 led3.write(1)
-                                print("last")
-                                #TODO send signal to REST endpoint
+                                purpleMessage=client.messages.create(
+                                        to=my_num,
+                                        from_=twilio_num,
+                                        body="You haven't worn this in more than a month. I'd suggest you wear this more or donate it."+str(time.ctime(int(time.time()))))
         else:
+            finalMessage=client.messages.create(
+                to=my_num,
+                from_=twilio_num,
+                body="After a very long time you're wearing it. hopefully you're either wearing it more or donating it. "
+            )
             green_led.write(0)
             yellow_led.write(0)
             led3.write(0)
